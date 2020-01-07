2127c3641bbed55a657f14d19b6b9f3002be20c1
Liew Chee Hin <cheehin98@gmail.com>
2020-01-07 10:20:33 +0800

    ver 6.1(JIngming)

--- app/src/main/java/com/example/volunteerassignment/ui/user_account/UserAccountFragment.kt
+++ app/src/main/java/com/example/volunteerassignment/ui/user_account/UserAccountFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager.widget.ViewPager
 import com.example.volunteerassignment.R
-import com.example.volunteerassignment.ui.user_point_and_redeem.UserPointAndRedeem_History
-import com.example.volunteerassignment.ui.user_point_and_redeem.UserPointAndRedeem_Reward
 import com.google.android.material.tabs.TabLayout
+import android.content.Intent
+import com.google.firebase.storage.FirebaseStorage
+import android.app.Activity.RESULT_OK
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import java.io.ByteArrayOutputStream
+import android.net.Uri
 
 /**
  * A simple [Fragment] subclass.
@@ -27,6 +32,13 @@ class UserAccountFragment : Fragment() {
     private lateinit var viewpager: ViewPager
     private lateinit var tabLayout: TabLayout
 
+
+    private lateinit var ProfileImg: ImageView
+
+    private val GALLERY_REQUEST_CODE = 1
+
+    private lateinit var storage: FirebaseStorage
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -38,11 +50,69 @@ class UserAccountFragment : Fragment() {
 
         tabLayout = root.findViewById(R.id.tabLayout)
         viewpager = root.findViewById(R.id.viewPager)
-
         setupPager()
         integrateTabWithPager()
+
+        ProfileImg = root.findViewById(R.id.profileImg)
+        ProfileImg.setOnClickListener{
+            pickFromGallery()
+        }
+
         return root
     }
+
+    private fun pickFromGallery() {
+
+        val intent = Intent(Intent.ACTION_PICK)
+
+        intent.type = "image/*"
+
+        val mimeTypes = arrayOf("image/jpeg", "image/png")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == GALLERY_REQUEST_CODE &&  data != null && data.getData() != null && resultCode == RESULT_OK){
+            val dataUri = data.data as Uri
+            //val bitmapImag = data.extras as Bitmap
+            ProfileImg.setImageURI(dataUri)
+
+            storage = FirebaseStorage.getInstance()
+
+            val storageRef = storage.reference
+
+            // Create a reference to "mountains.jpg"
+            val mountainsRef = storageRef.child("profile.jpg")
+
+            // Create a reference to 'images/mountains.jpg'
+            val mountainImagesRef = storageRef.child("images/profile.jpg")
+
+            // While the file names are the same, the references point to different files
+            mountainsRef.name == mountainImagesRef.name // true
+            mountainsRef.path == mountainImagesRef.path // false
+
+            val bitmap = (ProfileImg.drawable as BitmapDrawable).bitmap
+            val baos = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+            val data = baos.toByteArray()
+
+            var uploadTask = mountainsRef.putBytes(data)
+
+            uploadTask.addOnFailureListener {
+                // Handle unsuccessful uploads
+            }.addOnSuccessListener {
+                // taskSnapshot.metadata contains file metadata such as size, content-type, etc.
+                // ...
+            }
+
+        }
+    }
+
+
     private fun setupPager() {
         viewpager.setAdapter(fragmentPagerAdapter(childFragmentManager))
     }
